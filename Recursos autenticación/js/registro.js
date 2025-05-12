1aac7defd8e035fbe9be88f3ddfec01cfb62f7df
lauuurars <1120098177@u.icesi.edu.co>
2025-05-12 07:22:25 -0500

    se añadió carpeta

---
+++ Recursos autenticación/js/registro.js
@@ -0,0 +1,12 @@
+/** 
+ * REGISTRO:
+ * ---------
+ * 1. Capturar con un selector el formulario de registro.
+ * 2. Escuchar el evento submit del formulario
+ * 3. Capturar los valor de registro en constantes: nombre, apellido, etc... 
+ * 5. Definir un arreglo Usuarios que recupere los usuarios existentes en Local Storage
+ * 6. Buscar por email si ya es un Usuario Registrado 
+ * 7. Si ya está registrado mostrar una alerta, limpiar formurio y retornar
+ * 8. Si no lo está, guarda el nuevo usuario en localStorage. 
+ * 9. Redirige a la página de inicio de sesión tras un registro exitoso.
+ */
